ebaa94a9be394cec0b506bd26e4fee4750a9cfc5
SPlinterRed <131634018+SPlinterRed@users.noreply.github.com>
2025-11-13 10:05:56 +0800

    switched to v6 router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import { ConnectButton } from "thirdweb/react";
 import thirdwebIcon from "./thirdweb.svg";
 import { client } from "./client";
+import { Route, Router, Routes } from "react-router-dom";
 
 export function App() {
 	return (
-		<main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-			<div className="py-20">
-				<Header />
-
-				<div className="flex justify-center mb-20">
-					<ConnectButton
-						client={client}
-						appMetadata={{
-							name: "Example app",
-							url: "https://example.com",
-						}}
-					/>
-				</div>
-
-				<ThirdwebResources />
-			</div>
-		</main>
+		<>
+		<Routes>
+			{/* <Route path="/upload" element={}/> */}
+		</Routes>
+		</>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThirdwebProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </ThirdwebProvider>
   </React.StrictMode>
 );
